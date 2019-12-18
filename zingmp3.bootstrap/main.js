dc0a7d46d5e8fb134ca46ad80bafb236ff2780c8
qtv10291 <quangtuanvu1991@gmail.com>
2019-12-18 09:14:18 +0700

    lan1

---
+++ zingmp3.bootstrap/main.js
@@ -0,0 +1,9 @@
+
+var height_card_son = $('.card-son').width();
+$('.card-son').css({'height':height_card_son+'px'});
+var height_overlay_web = $('.overlay-web').width();
+$('.overlay-web').css({'height':height_overlay_web+'px'});
+var height_play_web = $('.play-web').width();
+$('.play-web').css({'height':height_play_web +'px'})
+var height_card_son1 = $('.card-son1').width();
+$('.card-son1').css({'height':height_card_son1*493/876+'px'});
