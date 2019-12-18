b0979b09e4bd674cd5ccf8158fb0ed1bfd92a3ba
qtv10291 <quangtuanvu1991@gmail.com>
2019-12-18 12:08:59 +0700

    lan2

--- zingmp3.bootstrap/main.js
+++ zingmp3.bootstrap/main.js
@@ -5,5 +5,20 @@ var height_overlay_web = $('.overlay-web').width();
 $('.overlay-web').css({'height':height_overlay_web+'px'});
 var height_play_web = $('.play-web').width();
 $('.play-web').css({'height':height_play_web +'px'})
+
 var height_card_son1 = $('.card-son1').width();
 $('.card-son1').css({'height':height_card_son1*493/876+'px'});
+
+var height_card_son2ver2 = $('.card-son2-2').width();
+$('.card-son2-2').css({'height':height_card_son2ver2*338/600+'px'});
+var height_overlay_web2ver2 = $('.overlay-web2-2').width();
+$('.overlay-web2-2').css({'height':height_overlay_web2ver2*338/600+'px'});
+var height_play_web2ver2 = $('.play-web2-2').width();
+$('.play-web2-2').css({'height':height_play_web2ver2*338/600 +'px'})
+
+var height_card_son2ver1 = $('.card-son2-1').width();
+$('.card-son2-1').css({'height':height_card_son2ver1*338/600+'px'});
+var height_overlay_web2ver1 = $('.overlay-web2-1').width();
+$('.overlay-web2-1').css({'height':height_overlay_web2ver1*338/600+'px'});
+var height_play_web2ver1 = $('.play-web2-1').width();
+$('.play-web2-1').css({'height':height_play_web2ver1*338/600 +'px'})
