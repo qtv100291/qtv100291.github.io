c370bed5a334e5996b90dc12759da32881323070
qtv91 <quangtuanvu1991@gmail.com>
2020-08-10 09:06:07 +0700

    update rock-paper-scissors

---
+++ rock-paper-scissors/precache-manifest.f26c8fc38a1f2b8e3e925fe08ff37b12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7288f7f48b49df2a2a30d05f6b0edfb5",
+    "url": "/qtv100291/qtv100291.github.io/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "1fcaf2022d2390b98e63",
+    "url": "/qtv100291/qtv100291.github.io/rock-paper-scissors/static/css/main.a54ff8a1.chunk.css"
+  },
+  {
+    "revision": "3f35651eb065259559e1",
+    "url": "/qtv100291/qtv100291.github.io/rock-paper-scissors/static/js/2.17ed1925.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qtv100291/qtv100291.github.io/rock-paper-scissors/static/js/2.17ed1925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fcaf2022d2390b98e63",
+    "url": "/qtv100291/qtv100291.github.io/rock-paper-scissors/static/js/main.b76a310d.chunk.js"
+  },
+  {
+    "revision": "fdb8ac5a5ec8716157cf",
+    "url": "/qtv100291/qtv100291.github.io/rock-paper-scissors/static/js/runtime-main.b8b440ee.js"
+  },
+  {
+    "revision": "9bbddf0c3d3d9baa80e7e7084207ac9c",
+    "url": "/qtv100291/qtv100291.github.io/rock-paper-scissors/static/media/rock.9bbddf0c.png"
+  },
+  {
+    "revision": "f4164fff21ea5ef90184f7729b2e8037",
+    "url": "/qtv100291/qtv100291.github.io/rock-paper-scissors/static/media/scissors.f4164fff.png"
+  }
+]);
