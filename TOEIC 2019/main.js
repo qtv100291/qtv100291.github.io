043a999a4b385085a7a561777105522a001edbe5
qtv91 <quangtuanvu1991@gmail.com>
2021-02-22 16:17:43 +0700

    update set current time

--- TOEIC 2019/main.js
+++ TOEIC 2019/main.js
@@ -5,10 +5,25 @@ for (let i = 1; i < 11; i++){
             Test - ${i}
         </div>`
 }
-console.log(output)
+if (!document.querySelector('.audio-player').getAttribute('src')) {
+    document.querySelector('.access-button').disabled = true
+}
 document.querySelector('.audio-list').innerHTML = output;
 function startAudio(index){
     document.querySelector('.audio-player').setAttribute('src', `MP3/Test ${index > 9 ? index : `0${index}`}.mp3`)  
     document.querySelector('.audio-player').play()
     document.querySelector('.title').textContent = `Đang phát : TEST - ${index}`
+    if (document.querySelector('.audio-player').getAttribute('src')) {
+        document.querySelector('.access-button').disabled = false
+    }
+    document.querySelector('#minute').value = ""
+    document.querySelector('#second').value = ""
+}
+
+document.querySelector('.access-button').addEventListener('click', changeCurrentTime)
+function changeCurrentTime () {
+    const minutes =  parseInt(document.querySelector('#minute').value)
+    const seconds = parseInt(document.querySelector('#second').value)
+    const currentTime = minutes*60 + seconds
+    document.querySelector('.audio-player').currentTime = currentTime
 }
